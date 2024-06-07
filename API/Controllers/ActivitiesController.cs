0334cde8f3a9566533dcbb6e8aad1cc6ca19339d
Nika Chikhladze <nick.chikhladze123@gmail.com>
2024-06-07 16:20:45 +0400

    Add project files.

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Diagnostics;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseAPIController
+{
+    private readonly DataContext _context;
+
+    public ActivitiesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        var activities = await _context.Activities.ToListAsync();
+        return Ok(activities);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+    {
+        var activity = await _context.Activities.FindAsync(id);
+        return Ok(activity);
+    }
+}
