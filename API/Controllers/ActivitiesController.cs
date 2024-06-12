df0bff3533440400e5616943a78513c09ead2673
Nika Chikhladze <nick.chikhladze123@gmail.com>
2024-06-12 19:00:29 +0400

    Changes

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,30 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
-using System.Diagnostics;
+﻿using Application.Activities;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 
 public class ActivitiesController : BaseAPIController
 {
-    private readonly DataContext _context;
-
-    public ActivitiesController(DataContext context)
-    {
-        _context = context;
-    }
 
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        var activities = await _context.Activities.ToListAsync();
-        return Ok(activities);
+        return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityById(Guid id)
     {
-        var activity = await _context.Activities.FindAsync(id);
-        return Ok(activity);
+        return await Mediator.Send(new Details.Query { Id = id });
     }
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity(Activity activity)
+    {
+        await Mediator.Send(new Create.Command { Activity = activity });
+        return Ok();
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+        activity.Id = id;
+        await Mediator.Send(new Edit.Command { Activity = activity });
+        return Ok();
+    }
+
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,49 +1,60 @@
+using Application.Activities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Application.Mapper;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddCors(opt =>
+internal class Program
 {
-    opt.AddPolicy("CorsPolicy", policy =>
+    private static void Main(string[] args)
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
-    });
-});
+        var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+        // Add services to the container.
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddCors(opt =>
+        {
+            opt.AddPolicy("CorsPolicy", policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
+        });
+
+        builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(List.Handler).Assembly));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseCors("CorsPolicy");
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        using var scope = app.Services.CreateScope();
+        var services = scope.ServiceProvider;
+        try
+        {
+            var context = services.GetRequiredService<DataContext>();
+            context.Database.Migrate();
+        }
+        catch (Exception ex)
+        {
+
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogError(ex, "An Error Has Occured During Migration");
+        }
+
+        app.Run();
+    }
 }
-
-app.UseCors("CorsPolicy");
-app.UseAuthorization();
-
-app.MapControllers();
-
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-try
-{
-    var context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
-}
-catch (Exception ex)
-{
-
-    var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An Error Has Occured During Migration");
-}
-
-app.Run();
